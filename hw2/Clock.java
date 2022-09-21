de3c559006a62b229f1756b0d15e0873377ad1aa
samdeustachio <113033651+samdeustachio@users.noreply.github.com>
2022-09-21 17:32:20 -0400

    Add files via upload

--- hw2/Clock.java
+++ hw2/Clock.java
@@ -3,25 +3,27 @@ package hw2;
 import java.util.Scanner;
 
 public class Clock {
-	private ClockAlarm clockAlarm = new ClockAlarm();
+	private ClockAlarm clockAlarm ;
 	private String currentTime = "";
 	private Scanner zildjian = new Scanner(System.in);
 	
 	public Clock(String cTime) {
 		this.currentTime = cTime;
+		
 	}
 	
 	public Clock(String cTime, String sAlarm) {
 		this.currentTime = cTime;
-		this.setAlarmTime(sAlarm);
+		clockAlarm = new ClockAlarm(sAlarm);
 	}
 	
-	public void setAlarmTime(String t) {
-		this.clockAlarm.addAlarmTime(t);
+	public void setAlarmTime(String s) {
+		this.clockAlarm.addAlarmTime(s);
 	}
 	
 	public void removeAlarmTime() {
 		this.clockAlarm.displayAlarmTime();
+		System.out.println();
 		System.out.println("Which alarm would you like to remove? (enter the alarm's number)");
 		int num = zildjian.nextInt();
 		this.clockAlarm.deleteAlarmTime(num);
--- hw2/TestHarness.java
+++ hw2/TestHarness.java
@@ -4,7 +4,14 @@ public class TestHarness {
 	public static void main(String[] args) {
 		Clock c1 = new Clock("10:23", "6:00");
 		c1.setAlarmTime("7:30");
+		c1.setAlarmTime("8:00");
+		c1.setAlarmTime("10:00");
+		c1.setAlarmTime("11:00");
+		
+		c1.removeAlarmTime();
 		c1.showAlarmTimes();
 		
+		System.out.println();
+		c1.print();
 	}
 }
